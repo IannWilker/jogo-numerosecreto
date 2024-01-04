efc0ef2aa4336d908555d51ff7d88f1ad570b364
Iann Wilker <138894047+IannWilker@users.noreply.github.com>
2024-01-04 13:20:12 -0300

    Update na validacao.js
    Foram adicionados novos detalhes na tela ao vencer o jogo.

--- app/validacao.js
+++ app/validacao.js
@@ -12,11 +12,12 @@ function verificaSeOChutePossuiUmValorValido(chute) {
         `
         document.body.style.backgroundColor = '#0F0F0F'
 
+        } else {
+            elementoChute.innerHTML += '<div>Valor inválido</div>'
+            return
         }
-    } else {
-        elementoChute.innerHTML += '<div>Valor inválido</div>'
-        return
-    }
+
+    } 
 
     if (numeroForMaiorOuMenorQueOValorPermitido(numero)){
         elementoChute.innerHTML += `<div>Valor inválido: Fale um número entre ${menorValor} e ${maiorValor}</div>
@@ -25,6 +26,10 @@ function verificaSeOChutePossuiUmValorValido(chute) {
     }
 
     if (numero === numeroSecreto){
+
+        document.body.style.backgroundColor ='#508D69'
+        document.body.style.color = '#FFFFFF'
+
         document.body.innerHTML =`
             <h2>Você acertou!</h2>
             <h3>o número secreto era ${numeroSecreto}</h3>
@@ -34,9 +39,9 @@ function verificaSeOChutePossuiUmValorValido(chute) {
     
 
     } else if (numero > numeroSecreto) {
-        elementoChute.innerHTML += '<div>O número secreto é menor <i class="fa-solid fa-down-long"></i></div>'
+        elementoChute.innerHTML += '<div class="dica">O número secreto é menor <i class="fa-solid fa-down-long"></i></div>'
     } else {
-        elementoChute.innerHTML += '<div>O número secreto é maior <i class="fa-solid fa-up-long"></i></div>'
+        elementoChute.innerHTML += '<div class="dica">O número secreto é maior <i class="fa-solid fa-up-long"></i></div>'
     }
 }
 
